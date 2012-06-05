a34d929b7af45357965b4881bb0229e5cbfc3752
RakhiMutha <rakhimutha@gmail.com>
2012-06-05 11:56:40 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfirstrails::Application.config.secret_token = '8c273001d397124c17f3d2fd0bd74b3e60c99967d2d0e10b0b300c44c935c9c65b240d6f0b89246743ab5a786d5c9daca001b054cc0b810f7153000ea31e5068'
